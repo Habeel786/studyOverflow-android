e777c764af43f649eb0c3aaaf36bbbfc735cf42b
Habeel <habeelhashmi786@gmail.com>
2020-07-15 11:51:16 +0530

    search feature added

--- lib/screens/allQuestions/showdata.dart
+++ lib/screens/allQuestions/showdata.dart
@@ -1,16 +1,15 @@
 import 'package:admob_flutter/admob_flutter.dart';
-import 'package:auto_size_text/auto_size_text.dart';
+import 'package:draggable_scrollbar/draggable_scrollbar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:studyoverflow/services/admob_service.dart';
 import 'package:studyoverflow/services/database.dart';
 import 'package:studyoverflow/models/descmodel.dart';
-import 'package:studyoverflow/screens/home/description.dart';
 import 'package:studyoverflow/shared/loading.dart';
 import 'package:studyoverflow/shared/nodatascreen.dart';
 
 
 class Demo extends StatefulWidget {
-  //final List sharedprefdata;
   final String subject;
   final String semester;
   final String stream;
@@ -23,6 +22,12 @@ class Demo extends StatefulWidget {
 }
 
 class _DemoState extends State<Demo> {
+  ScrollController _arrowsController = ScrollController();
+  List<Data> mydata=List();
+  List<Data> filteredMydata=[];
+  List<Data> tempdata=[];
+
+  bool isSearching=false;
   var _data;
   bool adFlag=false;
   @override
@@ -30,47 +35,103 @@ class _DemoState extends State<Demo> {
     // TODO: implement initState
     super.initState();
     Admob.initialize('ca-app-pub-9118153038397153~5910414684');
-    _data= DatabaseServices().dataAboutQues( widget.stream,widget.semester, widget.chapter,widget.subject);
+    _data= DatabaseServices().dataAboutQues(widget.stream,widget.semester,widget.chapter,widget.subject);
   }
   @override
   Widget build(BuildContext context) {
     String answer="";
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: Text('Questions'),
-        centerTitle: true,
-        backgroundColor: Color(0xffD76EF5),
-      ),
-      body: StreamBuilder(
+    return StreamBuilder(
         stream:_data,
         builder: (_ , snapshot){
-          List<Data> mydata= snapshot.data;
           if(snapshot.connectionState==ConnectionState.waiting){
             return Loading();
           }else{
-            return mydata.isEmpty?nothingToShow("Data is to be added",'assets/notfound.png'):
-            ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (_, index){
-                  if((index+1)%11==0){
-                    adFlag=true;
-                  }else{
-                    adFlag=false;
-                  }
-                  return  adFlag?AdmobService().listtileWithAd(index,context,mydata[index].answer,
-                    mydata[index].question,mydata[index].chapter,mydata[index].diagram,
-                    mydata[index].yearofrepeat,mydata[index].marks,mydata[index].postedBy,
-                    mydata[index].postedOn,
-                  ):AdmobService().listtileWithoutAd(index,context,mydata[index].answer,
-                    mydata[index].question,mydata[index].chapter,mydata[index].diagram,
-                    mydata[index].yearofrepeat,mydata[index].marks,mydata[index].postedBy,
-                    mydata[index].postedOn,
-                  );
-                });
+            mydata= snapshot.data;
+            filterthedata(String value){
+             setState(() {
+               tempdata=mydata.where((u) =>
+               (u.question.toLowerCase().contains(value))).toList();
+             });
+             print(tempdata);
+            }
+            return mydata.isEmpty?nothingToShow("Data is to be added",'assets/notfound.png'):Scaffold(
+              appBar: AppBar(
+                elevation: 0,
+                title: !isSearching?Text('Questions'):
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white70
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20,right: 20),
+                    child: TextField(
+                      onChanged: (string){
+                        filterthedata(string);
+                      },
+                      style: TextStyle(
+                          color: Colors.black
+                      ),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                          icon: Icon(Icons.search),
+                          hintText: "Search Question Here",
+                          hintStyle: TextStyle(
+                              color: Colors.grey[600]
+                          )
+                      ),
+                    ),
+                  ),
+                ),
+                actions: [
+                  isSearching?IconButton(
+                      icon: Icon(Icons.clear,color: Colors.white,),
+                      onPressed: (){
+                        setState(() {
+                          isSearching=false;
+                        });
+                      }): IconButton(
+                      icon: Icon(Icons.search,color: Colors.white,),
+                      onPressed: (){
+                        setState(() {
+                          isSearching=true;
+                        });
+                      })
+                ],
+                centerTitle: true,
+                backgroundColor: Color(0xffD76EF5),
+              ),
+              body: DraggableScrollbar.arrows(
+                controller: _arrowsController,
+                child: ListView.builder(
+                  controller: _arrowsController,
+                    itemCount: tempdata.isEmpty?mydata.length:filteredMydata.length,
+                    itemBuilder: (_, index){
+                    if(tempdata.isEmpty){
+                      filteredMydata=mydata;
+                    }else{
+                      filteredMydata=tempdata;
+                    }
+                      if((index+1)%11==0){
+                        adFlag=true;
+                      }else{
+                        adFlag=false;
+                      }
+                     return adFlag?AdmobService().listtileWithAd(index,context,filteredMydata[index].answer,
+                        filteredMydata[index].question,filteredMydata[index].chapter,filteredMydata[index].diagram,
+                        filteredMydata[index].yearofrepeat,filteredMydata[index].marks,filteredMydata[index].postedBy,
+                        filteredMydata[index].postedOn,
+                      ):AdmobService().listtileWithoutAd(index,context,filteredMydata[index].answer,
+                        filteredMydata[index].question,filteredMydata[index].chapter,filteredMydata[index].diagram,
+                        filteredMydata[index].yearofrepeat,filteredMydata[index].marks,filteredMydata[index].postedBy,
+                        filteredMydata[index].postedOn,
+                      );
+                    }
+                    ),
+              ),
+            );
           }
         },
-      ),
-    );
+      );
   }
 }
